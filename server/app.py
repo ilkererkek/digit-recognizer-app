d9d6a98521af39763fc23450e7ba384c5e752323
ilkererkek <ilker.erke12@gmail.com>
2022-12-26 02:41:27 +0300

    model updated

---
+++ server/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+import numpy as np
+
+from model.digit_recognizer import DigitRecognizerModel
+from functions.request_decoder import decode_request
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+print('Model Loading')
+model = DigitRecognizerModel('./ModelFiles/2cnramodel.h5')
+print('Model loaded')
+
+
+@app.route("/predict",methods=['POST'])
+@cross_origin()
+def test():
+    image = decode_request(request)
+    prediction = model(image)
+    response = jsonify({'Prediction': str(prediction)})
+    return response
+
+@app.route('/')
+@cross_origin()
+def home():
+    return app.send_static_file('index.html')
+
+
