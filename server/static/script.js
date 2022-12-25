26da05e9221af8dfa8a05989bb567b7c017f9505
AYDOLDI <43027050+AYDOLDI@users.noreply.github.com>
2022-12-25 16:12:05 +0300

    Develop-7892 Front side improved

--- server/static/script.js
+++ server/static/script.js
@@ -2,75 +2,142 @@
 
 
 window.addEventListener('load', function () {
-    // get the canvas element and its context
-    var canvas = document.getElementById('output');
-    var context = canvas.getContext('2d');
-    var buttonClear = this.document.getElementById('btn-clear');
-    context.lineWidth = 40;
 
-    context.fillStyle = "white";
-    context.drawStyle = "black"
+  // get the canvas element and its context
+  var canvas = document.getElementById('output');
+  var context = canvas.getContext('2d');
+  var buttonClear = this.document.getElementById('btn-clear');
+  context.lineWidth = 40
+  context.fillStyle = "white";
+  context.drawStyle = "black"
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  var isIdle = true
+
+  const clear = (e) => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.fillRect(0, 0, canvas.width, canvas.height);
-    var isIdle = true;
-
-
-    const clear = (e) => {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    };
-
-
-    function drawstart(event) {
-      context.beginPath();
-      context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
-      isIdle = false;
+  }
+  function drawstart(event) {
+    context.beginPath();
+    context.moveTo(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop);
+    isIdle = false;
+  }
+  function drawmove(event) {
+    if (isIdle) return;
+    const x = event.pageX - canvas.offsetLeft;
+    const y = event.pageY - canvas.offsetTop;
+    if(x > canvas.width || x < 0 || y > canvas.height || y < 0){
+      drawend()
     }
-    function drawmove(event) {
-      if (isIdle) return;
-      const x = event.pageX - canvas.offsetLeft;
-      const y = event.pageY - canvas.offsetTop;
-      if(x > canvas.width || x < 0 || y > canvas.height || y < 0){
-        drawend()
-      }
-      context.lineTo(x, y);
-      context.stroke();
-    }
-    async function drawend(event) {
-      if (isIdle) return;
-      drawmove(event);
-      isIdle = true;
+    context.lineTo(x, y);
+    context.stroke();
+  }
+  async function drawend(event) {
+    if (isIdle) return;
+    drawmove(event);
+    isIdle = true
+    let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'))
+    let formData = new FormData();
+    formData.append("image", imageBlob, "image.png");
 
-      let imageBlob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
+    let response = await fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        body: formData,
+    });
+    let result = await response.text()
+    console.log(result);
+  }
+  function touchstart(event) { 
+      drawstart(event.touches[0]) 
+  }
+  function touchmove(event) { 
+      drawmove(event.touches[0]); 
+      event.preventDefault(); 
+  }
+  async function touchend(event) { 
+      drawend(event.changedTouches[0]) 
+  }
+  canvas.addEventListener('touchstart', touchstart, false);
+  canvas.addEventListener('touchmove', touchmove, false);
+  canvas.addEventListener('touchend',touchend, false);        
+  canvas.addEventListener('mousedown', drawstart, false);
+  canvas.addEventListener('mousemove', drawmove, false);
+  canvas.addEventListener('mouseup', drawend, false)
+  buttonClear.addEventListener('click', clear)
 
-      let formData = new FormData();
-      formData.append("image", imageBlob, "image.png");
-      
-      let response = await fetch('http://localhost:5000/predict', {
-          method: 'POST',
-          body: formData,
-      });
-      let result = await response.text()
-      console.log(result);
-    }
-    function touchstart(event) { 
-        drawstart(event.touches[0]) 
-    }
-    function touchmove(event) { 
-        drawmove(event.touches[0]); 
-        event.preventDefault(); 
-    }
-    async function touchend(event) { 
-        drawend(event.changedTouches[0]) 
-    }
-  
-    canvas.addEventListener('touchstart', touchstart, false);
-    canvas.addEventListener('touchmove', touchmove, false);
-    canvas.addEventListener('touchend',touchend, false);        
-  
-    canvas.addEventListener('mousedown', drawstart, false);
-    canvas.addEventListener('mousemove', drawmove, false);
-    canvas.addEventListener('mouseup', drawend, false);
+  const navId = document.getElementById("nav_menu"),
+  ToggleBtnId = document.getElementById("toggle_btn"),
+  CloseBtnId = document.getElementById("close_btn");
 
-    buttonClear.addEventListener('click', clear)
-  
+  // ==== SHOW MENU ==== //
+  ToggleBtnId.addEventListener("click", () => {
+    navId.classList.add("show");
+  });
+
+  // ==== HIDE MENU ==== //
+  CloseBtnId.addEventListener("click", () => {
+    navId.classList.remove("show");
+  });
+
+  // ==== Animate on Scroll Initialize  ==== //
+  AOS.init();
+
+  // ==== GSAP Animations ==== //
+  // ==== LOGO  ==== //
+  gsap.from(".logo", {
+    opacity: 0,
+    y: -10,
+    delay: 1,
+    duration: 0.5,
+  });
+  // ==== NAV-MENU ==== //
+  gsap.from(".nav_menu_list .nav_menu_item", {
+    opacity: 0,
+    y: -10,
+    delay: 1.4,
+    duration: 0.5,
+    stagger: 0.3,
+  });
+  // ==== TOGGLE BTN ==== //
+  gsap.from(".toggle_btn", {
+    opacity: 0,
+    y: -10,
+    delay: 1.4,
+    duration: 0.5,
+  });
+  // ==== MAIN HEADING  ==== //
+  gsap.from(".main-heading", {
+    opacity: 0,
+    y: 20,
+    delay: 2.4,
+    duration: 1,
+  });
+  // ==== INFO TEXT ==== //
+  gsap.from(".info-text", {
+    opacity: 0,
+    y: 20,
+    delay: 2.8,
+    duration: 1,
+  });
+  // ==== CTA BUTTONS ==== //
+  gsap.from(".btn_wrapper", {
+    opacity: 0,
+    y: 20,
+    delay: 2.8,
+    duration: 1,
+  });
+  // ==== TEAM IMAGE ==== //
+  gsap.from(".team_img_wrapper img", {
+    opacity: 0,
+    y: 20,
+    delay: 3,
+    duration: 1,
+  });
   }, false); // end window.onLoad
+
+  function upload(){
+    var imgcanvas = document.getElementById("canv1");
+    var fileinput = document.getElementById("finput");
+    var image = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas);
+  }
